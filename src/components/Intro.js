b96450e93705bc4edb96122e257efc7577334a6d
tamimhossain11 <amdtamim3@gmail.com>
2025-02-01 21:41:34 +0600

    updated description

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -80,8 +80,8 @@ const Intro = () => {
             <SubBox>
                 <Text>
                     <h1>Hi,</h1>
-                    <h3>I'm CodeBucks.</h3>
-                    <h6>I design and Code simple yet beautiful websites.</h6>
+                    <h3>I'm Mahadir Islam Wafi.</h3>
+                    <h6></h6>
                 </Text>
             </SubBox>
             <SubBox>
